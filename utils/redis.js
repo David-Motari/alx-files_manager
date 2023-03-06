46f64aa6efe25bd6b2858553f7bfc326117a7f06
David Motari <david@Xubuntu.myguest.virtualbox.org>
2023-03-06 12:11:14 +0300

    Initial commit -m

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import redis from 'redis';
+import { promisify } from 'utils'
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.clientGet = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error}`);
+    });
+  }
+  is_alive() {
+    // returns true when the connection to Redis is a success otherwise, false
+    return this.client.connected;
+  }
+
+  async get(key) {
+    // takes a string key as argument and returns the Redis value stored for this key
+    const val = await this.clientGet(key);
+    return val;
+  }
+
+  async set(key, value, duration) {
+    // takes a string key, a value and a duration in second as arguments to store it in Redis (with an expiration set by the duration argument)
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    // takes a string key as argument and remove the value in Redis for this key
+    this.client.del(key);
+  }
+}
