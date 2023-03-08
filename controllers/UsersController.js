d371b2a06e8e1f6373264cefacfff70f1cd9882a
David Motari <david@Xubuntu.myguest.virtualbox.org>
2023-03-08 13:04:49 +0300

    user authentication

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
+import ObjectId from 'mongodb';
 import sha1 from 'sha1';
 import DBClient from '../utils/db';
+import RedisClient from '../utils/redis';
 
 class UsersController {
   static async postUsers(request, response) {
@@ -21,6 +23,26 @@ class UsersController {
     const shaUserPassword = sha1(uPassword);
     const result = await DBClient.db.collection('users').insertOne({ email: uEmail, password: shaUserPassword });
     return response.status(201).send({ id: result.insertedId, email: uEmail });
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token') || null;
+    if (!token) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const redisToken = await RedisClient.get(`auth_${token}`);
+    if (!redisToken) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(redisToken) });
+    if (!user) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+    delete user.password;
+
+    return response.status(200).send({ id: user._id, email: user.email });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 function Routes(app) {
   const router = express.Router();
@@ -14,6 +15,15 @@ function Routes(app) {
   });
   router.post('/users', (req, res) => {
     UsersController.postUsers(req, res);
+  });
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
   });
 }
 
